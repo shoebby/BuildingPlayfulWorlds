2318bf9f2800389c9357c295de1814fdd334b058
shoebby <thomas@vaneekelen.net>
2020-03-19 17:28:43 +0100

    tutorial area
    begin gemaakt met de tutorial area, bijna af alleen nog de hookshot hangar afmaken en een tutorial voor de boosters bedenken.

---
+++ eindopdracht_v1/Assets/interactibles/buttonHangar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonHangar : MonoBehaviour
+{
+    public GameObject door;
+    public bool doorIsOpening;
+
+    void Update()
+    {
+        if (doorIsOpening == true)
+        {
+            door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+
+        if (door.transform.position.y > 0.25f)
+        {
+            doorIsOpening = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        doorIsOpening = true;
+    }
+}
---
+++ eindopdracht_v1/Assets/interactibles/doorControllerTutArea.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorControllerTutArea : MonoBehaviour
+{
+    public GameObject door;
+    public bool doorIsOpening;
+
+    void Update()
+    {
+        if (doorIsOpening == true)
+        {
+            door.transform.Translate(Vector3.down * Time.deltaTime * 5);
+        }
+
+        if (door.transform.position.y < -5.5f)
+        {
+            doorIsOpening = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        doorIsOpening = true;
+    }
+}
