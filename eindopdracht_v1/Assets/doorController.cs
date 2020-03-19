001e04cb9bfdb70b54d4898d8db71d75de604f7b
shoebby <thomas@vaneekelen.net>
2020-03-19 13:46:18 +0100

    imported assets and begin level
    imported some low poly cars to try out and added some button stuff to use as a base for future puzzles.

---
+++ eindopdracht_v1/Assets/doorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorController : MonoBehaviour
+{
+    public GameObject door;
+    public bool doorIsOpening;
+
+    void Update()
+    {
+        if (doorIsOpening == true)
+        {
+            door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+
+        if (door.transform.position.y > 7f)
+        {
+            doorIsOpening = false;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        doorIsOpening = true;
+    }
+}
