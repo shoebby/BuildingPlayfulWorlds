63adba6c88b410babac5821c6557ca8f826361ce
shoebby <thomas@vaneekelen.net>
2020-03-25 01:15:17 +0100

    menus and timer
    added main menu, victory, defeat screens.
    
    Basic timer is in the game now, but needs some work to be fair and thrilling.

---
+++ eindopdracht_v1/Assets/timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+  
+    public Text counterText;
+    public float counter = 10f;
+    private float deathTime = 0f;
+
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+    }
+    void Update()
+    {
+        counter -= Time.deltaTime;
+        counterText.text = counter.ToString("0");
+
+        if (counter <= deathTime)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
+}
